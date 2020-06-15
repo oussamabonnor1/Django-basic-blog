540871518629f7bec778ce500666d247ce05b8e9
oussamabonnor1 <oussamabonnor@gmail.com>
2020-06-15 02:07:42 +0100

    adding post creation and edit forms

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<int:pk>', views.post_detail, name='post_detail')
+    path('post/<int:pk>', views.post_detail, name='post_detail'),
+    path('post/new', views.new_post, name='new_post'),
+    path('post/<int:pk>/edit', views.edit_post, name='edit_post'),
 ]
